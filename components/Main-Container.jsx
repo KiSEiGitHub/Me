7121b34da96fdca9c5611bff0bb827e244c96b0d
Tom <tommusique974@gmail.com>
2022-03-11 21:23:40 +0100

    feat(HardwareSeven) : Timer

--- components/Main-Container.jsx
+++ components/Main-Container.jsx
@@ -1,27 +1,23 @@
 import React from 'react';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
+import {useRouter} from 'next/router';
 import Nav from "./Nav";
 import {useMediaQuery} from "@chakra-ui/react";
 import NavMobile from "./NavMobile";
 
 const MainContainer = (props) => {
-    
+
     const router = useRouter();
     const [flip] = useMediaQuery('(min-width: 540px)')
 
     return (
         <>
             <Head>
-                <title>Tom - {router.asPath}</title>
-                <meta name = "viewport" content = "width=device-width, initial-scale=1.0" />
-                <link rel = "icon" href = "/Image/Fav.png" />
+                <title>Tom - {router.pathname.split('/')[1]}</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                <link rel="icon" href="/Image/Fav.png"/>
             </Head>
-            {flip
-                ? <Nav />
-                : <NavMobile />
-            }
-
+            {flip ? <Nav/> : <NavMobile/>}
             {props.children}
         </>
     );
--- pages/Projets/HardwareSeven.jsx
+++ pages/Projets/HardwareSeven.jsx
@@ -1,62 +1,90 @@
-import React, { useContext } from 'react';
-import { Box, Container, ListItem, Tag, Text, UnorderedList, useColorMode } from '@chakra-ui/react';
+import React, {useContext, useEffect, useRef, useState} from 'react';
+import {Box, Container, ListItem, Tag, Text, UnorderedList, useColorMode} from '@chakra-ui/react';
 import Section from '../../Layout/Section';
-import { ExtLink, ParaIndent, SectionTitle } from '../../Layout/CustomText';
-import { LangContext } from '../../Hook/LangContext';
-import { TextContext } from '../../Hook/TextContext';
+import {ExtLink, ParaIndent, SectionTitle} from '../../Layout/CustomText';
+import {LangContext} from '../../Hook/LangContext';
+import {TextContext} from '../../Hook/TextContext';
 import BtnReturnPro from '../../components/BtnReturnPro';
 
 const HardwareSeven = () => {
-    
-    const { toggleLang } = useContext(LangContext);
-    const { DataProject } = useContext(TextContext);
-    const { colorMode } = useColorMode();
-    
+
+    const {toggleLang} = useContext(LangContext);
+    const {DataProject} = useContext(TextContext);
+    const {colorMode} = useColorMode();
+
+    const [timerDays, setTimerDays] = useState('00');
+
+    let interval = useRef();
+
+    const starttimer = () => {
+        const EndDate = new Date('March 10, 2023 00:00:00');
+        interval = setInterval(() => {
+            const now = new Date().getTime();
+            const distance = EndDate - now;
+
+            const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+            if(distance < 0) {
+                clearInterval(interval.current);
+            } else {
+                setTimerDays(days);
+            }
+
+        })
+    }
+
+    useEffect(() => {
+        starttimer();
+        return () => {
+            clearInterval(interval.current);
+        }
+    });
+
     return (
         <>
-            <Container variant = "Main">
-                <Section delay = {0.2}>
+            <Container variant="Main">
+                <Section delay={0.2}>
                     <SectionTitle>HardwareSeven</SectionTitle>
-                    <Tag mb = {2}>2022</Tag>
-                    <Box mb = {2}>
-                        <Tag mr = {2}>Platforme</Tag>
+                    <Tag mb={2}>2022</Tag>
+                    <Box mb={2}>
+                        <Tag mr={2}>Platforme</Tag>
                         <span>PC / Mobile</span>
                     </Box>
-                    <Box mb = {4}>
-                        <Tag mr = {2}>Stack</Tag>
+                    <Box mb={4}>
+                        <Tag mr={2}>Stack</Tag>
                         <span>Prestashop</span>
                     </Box>
-                    <Box mb = {4}>
-                        <Tag mr = {2}>Site</Tag>
-                        <ExtLink href = "https://hardwareseven.fr/index.php">
+                    <Box mb={4}>
+                        <Tag mr={2}>Site</Tag>
+                        <ExtLink href="https://hardwareseven.fr/index.php">
                             HardwareSeven
                         </ExtLink>
                         <Text
-                            d = "inline"
-                            color = {colorMode === 'light' ? '#928c7b' : '#535355'}
-                            fontStyle = "italic"
-                            p = "0 5px"
+                            d="inline"
+                            color={colorMode === 'light' ? '#928c7b' : '#535355'}
+                            fontStyle="italic"
+                            p="0 5px"
                         >
-                            en ligne jusqu&apos;a Mars 2023
+                            en ligne pendant {timerDays} jours
                         </Text>
                     </Box>
                 </Section>
-                
-                <Section delau = {0.3}>
+
+                <Section delau={0.3}>
                     <SectionTitle>Description</SectionTitle>
                     <ParaIndent>
                         HardwareSeven est un site d&apos;ecommerce utilisant le CDN
                         {' '}
-                        <ExtLink href = {'https://www.prestashop.com/fr'}>
+                        <ExtLink href={'https://www.prestashop.com/fr'}>
                             prestashop
                         </ExtLink>.
                     </ParaIndent>
                 </Section>
-                
-                <Section delay = {0.4}>
+
+                <Section delay={0.4}>
                     <SectionTitle>{toggleLang ? 'Tâches' : 'Tasks'}</SectionTitle>
                     <ParaIndent>
-                        {DataProject[ 'HardwareSeven' ][ 'Tache' ][ 'fr' ]}
+                        {DataProject['HardwareSeven']['Tache']['fr']}
                         <UnorderedList>
                             <ListItem>Gestion des comptes pour les clients</ListItem>
                             <ListItem>Gestions des comptes administrateur</ListItem>
@@ -68,8 +96,8 @@ const HardwareSeven = () => {
                         </UnorderedList>
                     </ParaIndent>
                 </Section>
-                <Section delay = {0.5}>
-                    <BtnReturnPro />
+                <Section delay={0.5}>
+                    <BtnReturnPro/>
                 </Section>
             </Container>
         </>
